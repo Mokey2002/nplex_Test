07828f51dc25f33696a50dcc07efd8ef4d7fcd2a
Mokey2002 <edventura91@gmail.com>
2024-12-04 12:46:55 -0800

    added frontend to read data from endpoint

--- frontend/src/lib/dataset.ts
+++ frontend/src/lib/dataset.ts
@@ -5,10 +5,31 @@ type Dataset = {
   id: number;
   name: string;
 };
+type UmaSetPoint={
+  id: number;
+  x_coor: number;
+  y_coor: number;
+  sample_id:number;
+
+}
+
 
 export function useDatasets() {
   const { data, error, isLoading } = useSWR<Dataset[]>(
     "/api/dataset/",
+    fetchJson
+  );
+
+  return {
+    datasets: data,
+    isLoading,
+    isError: error
+  };
+}
+
+export function useUmaSetPoints() {
+  const { data, error, isLoading } = useSWR<UmaSetPoint[]>(
+    "/api/UmaPlotPoint/?id=3",
     fetchJson
   );
 
